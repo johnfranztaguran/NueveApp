51b56ae58795a89ff6d2e5a9fac4a8d6c4527e40
John Franz Taguran <johnfranztaguran@gmail.com>
2026-01-02 00:42:58 +0800

    fix babel issue and tailwind issue

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,12 @@
-// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-// /**
-//  * Metro configuration
-//  * https://reactnative.dev/docs/metro
-//  *
-//  * @type {import('@react-native/metro-config').MetroConfig}
-//  */
-// const config = {};
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-const { getDefaultConfig } = require("@expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: './global.css' });
